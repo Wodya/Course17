7f259081c1186e31d8935c3a1d2e6d5e532adcff
Oleg Bogdasarov <o.bogdasarov@gmail.com>
2021-08-14 17:50:54 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import MessageComp from './Components/MessageComp'
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [count, setCount] = useState(0);
+
+  console.log(count);
+  console.log('Запуск');
+  const onInputChanged = e =>
+  {
+    setInputText(e.target.value);
+    setCount(prev => prev + 1);
+  };
+
   return (
     <div>
-      <input type="text" value={inputText} onChange = {e => setInputText(e.target.value)}/>
+      <input type="text" value={inputText} onChange = {onInputChanged}/>
         <MessageComp showText={inputText} />
     </div>
   );
